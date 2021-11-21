0f268b1021826e1e6241d475b0d4abe43a8b5eae
romalynfloranda <romalynfloranda0203@gmail.com>
2021-11-21 19:34:42 +0800

    The program is working.

--- program2.py
+++ program2.py
@@ -19,8 +19,58 @@ def getQuestions (addend, augend):
     return _question
 
 def correctAnswers (addend1, augend1):
-    _result = addend1 + augend1
+    _result = int (addend1 + augend1)
     return _result
+
+def resultSummary(_score):
+    print (f"You got {_score} out of 10 questions correct.")
+
+def pointing ():
+    _right = int (1)
+    _wrong = int (0)
+    return _right, _wrong
+
+def qOne():
+    if firstQ == resultAB:
+        return right
+    else: return wrong
+def qTwo():
+    if secondQ == resultCD:
+        return right
+    else: return wrong
+def qThree():
+    if thirdQ == resultEF:
+        return right
+    else: return wrong
+def qFour():
+    if fourthQ == resultGH:
+        return right
+    else: return wrong
+def qFive():
+    if fifthQ == resultIJ:
+        return right
+    else: return wrong
+def qSix():
+    if sixthQ == resultKL:
+        return right
+    else: return wrong
+def qSeven():
+    if seventhQ == resultMN:
+        return right
+    else: return wrong
+def qEight():
+    if eighthQ == resultOP:
+        return right
+    else: return wrong
+def qNine():
+    if ninthQ == resultQR:
+        return right
+    else: return wrong
+def qTen():
+    if tenthQ == resultST:
+        return right
+    else: return wrong
+
 
 # variables for 20 random numbers
 a = getRandomNumbers ()
@@ -69,3 +119,19 @@ resultOP = correctAnswers (o,p)
 resultQR = correctAnswers (q,r)
 resultST = correctAnswers (s,t)
 
+# result score
+right, wrong = pointing ()
+A = qOne ()
+B = qTwo ()
+C = qThree ()
+D = qFour ()
+E = qFive ()
+F = qSix ()
+G = qSeven ()
+H = qEight ()
+I = qNine ()
+J = qTen ()
+
+#other
+score = A + B + C + D + E + F + H + J
+resultSummary (score)
