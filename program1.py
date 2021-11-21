fcf5bb328c043b9f00a0631d8d8668cb6c0731a1
romalynfloranda <romalynfloranda0203@gmail.com>
2021-11-21 14:54:40 +0800

    The program is complete and working.

--- program1.py
+++ program1.py
@@ -61,50 +61,48 @@ def getNumberSorter(a, b, c, d):
                                 if d >= c >= a:
                                     return b, d, c, a   
         else: # c ang highest
-            if b >= a and b >= c and b >= d:
-                #a>c>d
-                if a >= c >= d:
-                    return b, a, c, d
-                else: #a>d>c
-                    if a >= d >= c:
-                        return b, a, d, c
-                    else: #c>a>d 
-                        if c >= a >= d:
-                            return b, c, a, d
-                        else: #c>d>a 
-                            if c >= d >= a:
-                                return b, c, d, a
-                            else: #d>a>c
-                                if d >= a >= c:
-                                    return b, d, a, c
-                                else:   #d>c>a
-                                    if d >= c >= a:
-                                        return b, d, c, a   
+            if c >= a and c >= b and c >= d:
+                #a>b>d
+                if a >= b >= d:
+                    return c, a, b, d
+                else: #a>d>b
+                    if a >= d >= b:
+                        return c, a, d, b
+                    else: #b>a>d 
+                        if b >= a >= d:
+                            return c, b, a, d
+                        else: #b>d>a 
+                            if b >= d >= a:
+                                return c, b, d, a
+                            else: #d>a>b
+                                if d >= a >= b:
+                                    return c, d, a, b
+                                else:   #d>b>a
+                                    if d >= b >= a:
+                                        return c, d, b, a
             else: # d ang highest
-                if b >= a and b >= c and b >= d:
-                     #a>c>d
-                    if a >= c >= d:
-                        return b, a, c, d
-                    else: #a>d>c
-                        if a >= d >= c:
-                            return b, a, d, c
-                        else: #c>a>d 
-                            if c >= a >= d:
-                                return b, c, a, d
-                            else: #c>d>a 
-                                if c >= d >= a:
-                                    return b, c, d, a
-                                else: #d>a>c
-                                    if d >= a >= c:
-                                        return b, d, a, c
-                                    else:   #d>c>a
-                                        if d >= c >= a:
-                                            return b, d, c, a  
-
+                if d >= a and d >= b and d >= c:
+                     #a>b>c
+                    if a >= b >= c:
+                        return d, a, b, c
+                    else: #a>c>b
+                        if a >= c >= b:
+                            return d, a, c, b
+                        else: #b>a>c 
+                            if b >= a >= c:
+                                return d, b, a, c
+                            else: #b>c>a 
+                                if b >= c >= a:
+                                    return d, b, c, a
+                                else: #c>a>b
+                                    if c >= a >= b:
+                                        return d, c, a, b
+                                    else:   #c>b>a
+                                        if c >= b >= a:
+                                            return d, c, b, a  
 
 # Print the 4 numbers from highest to lowest using only if-else statement.
 first, second, third, fourth = getNumber()
 arrange = getNumberSorter (first, second, third, fourth)
 display (arrange)
 
-# function to na gagawin ko mamaya >> arrange (first, second, third, fourth)
