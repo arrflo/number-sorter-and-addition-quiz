3f18b9ecad029aeaed0adf690845992698724893
romalynfloranda <romalynfloranda0203@gmail.com>
2021-11-21 17:55:27 +0800

    The program is generating numbers.

---
+++ program2.py
@@ -0,0 +1,54 @@
+# Program 2: Addition Quiz
+# Create a program that automatically generate two random numbers to add (0 to 99)
+# Let the user answer and evaluate if the user has the correct answer
+# The program will ask the user 10 addition operation
+# Display the result summary of the 10 operations (ex 9/10)
+
+import random
+
+def getRandomNumbers ():
+    randomm = random.randint(0, 99)
+    return randomm
+
+def header ():
+    print ("Addition Quiz")
+    print ("Answer the following questions correctly.")
+
+def getQuestions (addend, augend):
+    _question = int(input (f"{addend} + {augend} ="))
+    return _question
+
+# variables for 20 random numbers
+a = getRandomNumbers ()
+b = getRandomNumbers ()
+c = getRandomNumbers ()
+d = getRandomNumbers ()
+e = getRandomNumbers ()
+f = getRandomNumbers ()
+g = getRandomNumbers ()
+h = getRandomNumbers ()
+i = getRandomNumbers ()
+j = getRandomNumbers ()
+k = getRandomNumbers ()
+l = getRandomNumbers ()
+m = getRandomNumbers ()
+n = getRandomNumbers ()
+o = getRandomNumbers ()
+p = getRandomNumbers ()
+q = getRandomNumbers ()
+r = getRandomNumbers ()
+s = getRandomNumbers ()
+t = getRandomNumbers ()
+
+# 10 questions
+header ()
+firstQ = getQuestions (a,b)
+secondQ = getQuestions (c,d)
+thirdQ = getQuestions (e,f)
+fourthQ = getQuestions (g,h)
+fifthQ = getQuestions (i,j)
+sixthQ = getQuestions (k,l)
+seventhQ = getQuestions (m,n)
+eighthQ = getQuestions (o,p)
+ninthQ = getQuestions (q,r)
+tenthQ = getQuestions (s,t)
